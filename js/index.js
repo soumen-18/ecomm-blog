5ba325900cb1374eada5d6dffe054a7656d5624a
soumen-18 <132756613+soumen-18@users.noreply.github.com>
2025-01-20 15:31:57 +0530

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const absolutePath = 'file:///C:/Users/user/Desktop/my-ecomm-blog';
+let allProducts = [];
+
+(function() {
+    // Define a list of pages that should trigger the redirect if logged in
+    const redirectPages = ['login.html', 'register.html'];
+
+    // Get the current page's filename
+    const currentPage = window.location.pathname.split('/').pop(); // Extract filename from URL
+
+    // Check if the current page is in the redirectPages list and if the user is logged in
+    if (redirectPages.includes(currentPage) && localStorage.getItem('checkLogged') === 'true') {
+        // Redirect to the shop page if the user is logged in
+        window.location.href = 'index.html';  // Ensure this is the correct path to your shop page
+    }
+})();
+
+// Start to Fetch all products from FakeStore API
+async function fetchAllProducts() {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        allProducts = await response.json();
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+}
+// End to Fetch all products from FakeStore API
+
+function display_product(fetch_id) {
+    localStorage.setItem('clickedProduct', fetch_id)
+    // window.location.href = `${absolutePath}/details.html?id=${fetch_id}`;
+    window.location.href = `${absolutePath}/details.html`;
+}
+
+function add_in_cart(product_id) {
+    alert('This function will implement soon!');
+}
+
